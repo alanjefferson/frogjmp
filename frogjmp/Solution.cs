adfad0fc4dc0f5c582921d1d0efad9d993211d47
Alan Jefferson Nascimento <alan.jefferson@outlook.com>
2022-04-22 11:58:40 -0300

    Add project to repository

---
+++ frogjmp/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frogjmp
+{
+	public class Solution
+	{
+		public int solution(int X, int Y, int D) {
+			double result = ((double)Y - (double)X) / (double)D;
+			var count = result % 1 == 0 ? (int)result : result + 1;
+
+			return (int)count;
+		}
+	}
+}
